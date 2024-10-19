7acf1fd8a0b0191c337a2c0a0bbc0d5eb9845a11
arathiponneth <qbj2sw@virginia.edu>
2024-10-19 19:36:04 -0400

    added calendar icon

--- gwh24/src/Header.jsx
+++ gwh24/src/Header.jsx
@@ -1,11 +1,14 @@
 import './Header.css'
 import flower from "./Flower.svg";
+import calendar from "./calendar.jpg";
+
 
 function Header() {
     return (
         <div class="navBar">
             <a href = ".\App.jsx" class="logo">Path to Bloom</a>
-            <img src={flower} alt="" />
+            <img src={flower} alt="" class= "flowerpic"/>
+            <img src={calendar} alt="" class = "calendar" />
         </div>
     );
 }
