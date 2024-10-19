0cc1a274c80a253ada6cafe730a8edf85ba2c04d
pallavim8 <103335683+pallavim8@users.noreply.github.com>
2024-10-19 17:50:46 -0400

    header jsx and css

---
+++ gwh24/src/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <div class = "navBar">
+         <head>
+            <h1>Path to Bloom</h1>
+         </head>
+        </div>
+    )   
+}
+
+export default Header
