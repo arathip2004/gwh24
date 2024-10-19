6dc94528738ee2efb201bbf8cdb032205f8bde0c
arathiponneth <qbj2sw@virginia.edu>
2024-10-19 11:47:20 -0400

    added flowers yay

---
+++ gwh24/src/FlowerFooter.jsx
@@ -0,0 +1,52 @@
+import './FlowerFooter.css'
+
+function FlowerFooter() {
+
+  return (
+    <>
+      <div class="box">
+  <div class="plant-box"></div>
+  
+  <div class="flower -flower1">
+    <div class="plant"></div>
+  </div>
+  
+  <div class="flower -flower2">
+    <div class="plant"></div>
+  </div>
+  
+  <div class="flower -flower3">
+    <div class="plant"></div>
+  </div>
+
+  <div class="flower -flower4">
+    <div class="plant"></div>
+  </div>
+
+  <div class="flower -flower5">
+    <div class="plant"></div>
+  </div>
+
+  <div class="flower -flower6">
+    <div class="plant"></div>
+  </div>
+
+  <div class="flower -flower7">
+    <div class="plant"></div>
+  </div>
+
+  <div class="flower -flower8">
+    <div class="plant"></div>
+  </div>
+
+  <div class="flower -flower9">
+    <div class="plant"></div>
+  </div>
+
+</div>
+
+    </>
+  )
+}
+
+export default FlowerFooter
--- gwh24/src/main.jsx
+++ gwh24/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import FLowerFooter from './FlowerFooter.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <FLowerFooter />
   </StrictMode>,
 )
