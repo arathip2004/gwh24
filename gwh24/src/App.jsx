ff3aced6d57a26dbcc988e27772fea4ab9956d94
pallavim8 <103335683+pallavim8@users.noreply.github.com>
2024-10-19 11:36:52 -0400

    created search bar for main page

--- gwh24/src/App.jsx
+++ gwh24/src/App.jsx
@@ -5,11 +5,9 @@ function App() {
 
   return (
     <>
-      <h1>GWH 2024</h1>
-      
-      <p className="read-the-docs">
-        this is our project yay
-      </p>
+      <div class="input-wrapper">
+        <input type="text" placeholder="Type here..." name="text" class="input" />
+      </div>
     </>
   )
 }
