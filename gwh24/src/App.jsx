21ea15590313af344d7c311febba3b784e1912fb
arathiponneth <qbj2sw@virginia.edu>
2024-10-19 12:21:01 -0400

    made userInput into variable

--- gwh24/src/App.jsx
+++ gwh24/src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 
 function App() {
 
+  const [userInput, setUserInput] = useState('');
+
   return (
     <>
       <h1>
         How are you feeling today?
       </h1>
       <div class="input-wrapper">
-        <input type="text" placeholder="Type here..." name="text" class="input" />
+        <input type="text" placeholder="Type here..." name="text" class="input"  value={userInput} onChange={e => setUserInput(e.target.value)} />
       </div>
     </>
   )
