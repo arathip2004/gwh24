69a111467059033633ebfc84c5bf545e1c0330ad
athiraponneth <ammu@Athiras-MacBook-Air.local>
2024-10-19 14:03:41 -0400

    Input button

--- gwh24/src/App.jsx
+++ gwh24/src/App.jsx
@@ -30,10 +30,31 @@ function App() {
       <h1>
         How are you feeling today?
       </h1>
-      <div class="input-wrapper">
-        <input type="text" placeholder="Type here..." name="text" class="input"  value={userInput} onChange={e => setUserInput(e.target.value)}/>
-        <button onClick={handleSubmit}>Submit</button> {/* Button to trigger submission */}      
-      </div>
+      <div class = "container">
+      <div class="search">
+  <input placeholder="Search" class="search__input" type="text" onKeyDown={(e) => {
+    if (e.key === "Enter" || e.key === "Return") {
+      handleSubmit();
+    }}}/>
+  <button class="search__button" onClick={handleSubmit}>
+    <svg
+      viewBox="0 0 16 16"
+      class="bi bi-arrow-right"
+      fill="currentColor"
+      height="16"
+      width="16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M10.854 8.354a.5.5 0 0 0 0-.708l-4-4a.5.5 0 1 0-.708.708L9.293 7.5H2.5a.5.5 0 0 0 0 1h6.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4z"
+      />
+    </svg>
+  </button>
+</div>
+</div>
+
+
       {result !== null && (
         <div>
           <p>Sentiment Score: {result}</p>
