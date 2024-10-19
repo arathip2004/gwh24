45338985779a4b563678c76a735ecf5b098a1157
arathiponneth <qbj2sw@virginia.edu>
2024-10-19 19:17:35 -0400

    added logo

--- gwh24/src/Header.jsx
+++ gwh24/src/Header.jsx
@@ -1,9 +1,11 @@
 import './Header.css'
+import flower from "./Flower.svg";
 
 function Header() {
     return (
         <div class="navBar">
             <a href = ".\App.jsx" class="logo">Path to Bloom</a>
+            <img src={flower} alt="" />
         </div>
     );
 }
