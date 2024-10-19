48e812c88f44f8ffd4394fd1cb381a153ee091ba
arathiponneth <qbj2sw@virginia.edu>
2024-10-19 11:48:51 -0400

    added how are you feeling

--- gwh24/src/App.jsx
+++ gwh24/src/App.jsx
@@ -5,6 +5,9 @@ function App() {
 
   return (
     <>
+      <h1>
+        How are you feeling today?
+      </h1>
       <div class="input-wrapper">
         <input type="text" placeholder="Type here..." name="text" class="input" />
       </div>
